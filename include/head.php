eba950e1c3166026cc9e60630a72067569258346
Damien <jamet.damien02@gmail.com>
2022-07-04 07:04:03 +1100

    favicon

--- include/head.php
+++ include/head.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8">
 <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
 <?php 
 session_start();
 if (!isset($_SESSION['email']) && !isset($_SESSION['nom']) && !isset($_SESSION['prenom'])) {
