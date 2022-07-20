4cd6bb542afaea3a169973242da8092869dd5416
Damien <jamet.damien02@gmail.com>
2022-07-20 14:24:29 +1100

    Update supprimer.php

--- fonctions/supprimer.php
+++ fonctions/supprimer.php
@@ -8,7 +8,7 @@ if(isset($_POST['delete']))
 {
     $id = $_POST['id'];
 
-    $query = "DELETE * FROM produit WHERE id='$id'";
+    $query = "DELETE FROM produit WHERE id='$id'";
     $query_run = mysqli_query($db, $query);
     
     if($query_run)
